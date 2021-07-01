5a5830dec3f8f9de222a8be38baba270f42d5119
maha <maha@gmail.com>
2021-07-01 07:26:35 +0530

    Java ClassCreation

---
+++ July20216_30AM_JavaBatch/src/com/icici/loans/carloans/ThirdClass.java
@@ -0,0 +1,32 @@
+package com.icici.loans.carloans;
+
+public class ThirdClass 
+{
+	// Data Properties
+	int a =10, b=20, result;
+	
+	// Behaviours
+	public void add()
+	{
+		result = a+b;
+		System.out.println("Add of A & B is : " + result);
+	}
+	
+	public void sub()
+	{
+		result = a-b;
+		System.out.println("Sub of A & B is : " + result);
+	}
+	
+	public static void main(String[] args) 
+	{		
+		ThirdClass obj = new ThirdClass();
+		obj.add();
+		obj.sub();
+		
+		ThirdClass obj1 = new ThirdClass();
+		obj1.add();
+		obj1.sub();
+	}
+
+}
