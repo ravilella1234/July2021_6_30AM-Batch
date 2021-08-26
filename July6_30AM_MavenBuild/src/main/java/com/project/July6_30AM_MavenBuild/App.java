3d8a25fff31b3cdade4ebae420cad11a4959522f
maha <maha@gmail.com>
2021-08-26 07:42:04 +0530

    Added Maven Project into Github

---
+++ July6_30AM_MavenBuild/src/main/java/com/project/July6_30AM_MavenBuild/App.java
@@ -0,0 +1,13 @@
+package com.project.July6_30AM_MavenBuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ July6_30AM_MavenBuild/src/main/java/com/project/July6_30AM_MavenBuild/ExtentManager.java
@@ -0,0 +1,31 @@
+package com.project.July6_30AM_MavenBuild;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentManager 
+{
+	static ExtentHtmlReporter htmlReport;
+	static ExtentReports report;
+	
+	public static ExtentReports getInstance()
+	{
+		if(htmlReport==null)
+		{
+			htmlReport = new ExtentHtmlReporter("D:\\AprilWorkSpace\\July6_30AMSeleniumBatch\\HtmlReports\\report.html");
+			htmlReport.config().setDocumentTitle("Automation Report");
+			htmlReport.config().setReportName("Functional Testing Report");
+			htmlReport.config().setTheme(Theme.STANDARD);
+		
+			report = new ExtentReports();
+			report.attachReporter(htmlReport);
+			report.setSystemInfo("OS", "Windows10");
+			report.setSystemInfo("Tester Name", "Ravikanth lella");
+			report.setSystemInfo("Browser", "Chrome");
+		}
+		
+		return report;
+	}
+
+}
