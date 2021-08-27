74a9c2c4b343c05617f5fd52727f6b9072b97bc3
maha <maha@gmail.com>
2021-08-27 07:32:48 +0530

    TestNG Day-1

---
+++ July6_30AM_MavenBuild/src/test/java/com/project/July6_30AM_MavenBuild/NewTest.java
@@ -0,0 +1,13 @@
+package com.project.July6_30AM_MavenBuild;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+	
+  @Test
+  public void f() 
+  {
+	  System.out.println("F test");
+  }
+}
